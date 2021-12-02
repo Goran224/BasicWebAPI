a534c391ca2a14e35fb29e68d1a7a6f7a3a04b24
Goran Trajkoski <goran.trajkoski97@gmail.com>
2021-12-02 21:00:48 +0100

    Contact Initial

---
+++ Migrations/20211202200013_Contact.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BasicWebAPI.Migrations
+{
+    public partial class Contact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContactName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CompanyId = table.Column<int>(type: "int", nullable: false),
+                    CountryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "CompanyId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Countries_CountryId",
+                        column: x => x.CountryId,
+                        principalTable: "Countries",
+                        principalColumn: "CountryId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_CompanyId",
+                table: "Contacts",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_CountryId",
+                table: "Contacts",
+                column: "CountryId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
---
+++ Models/Contact.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicWebAPI.Models
+{
+    public class Contact
+    {
+        public int ContactId { get; set; }
+        public string ContactName { get; set; }
+
+       
+        public int CompanyId { get; set; }
+        public int CountryId { get; set; }
+
+        [ForeignKey("CompanyId")]
+        public Company company { get; set; }
+
+        [ForeignKey("CountryId")]
+        public Country country { get; set; }
+    }
+}
